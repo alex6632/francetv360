7b6ed0819210d3e22ea3b2e5bda2bbd5b9d826a1
Alexandre Simonin <a.simonin69@gmail.com>
2017-06-19 17:39:20 +0200

    Update home

--- web/Views/inc/head.php
+++ web/Views/inc/head.php
@@ -5,6 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app/css/app.css">
+
+    <?php if($template == 'home') { ?>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+        <link type="text/css" rel="stylesheet" href="css/hackIT.css"  media="screen,projection"/>
+    <?php } else { ?>
+        <link rel="stylesheet" href="app/css/app.css">
+    <?php } ?>
+
     <title>France•TV360</title>
 </head>

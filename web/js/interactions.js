1bf755524973a3354a7211219a97b3398771daf5
Alexandre Simonin <a.simonin69@gmail.com>
2017-06-26 00:56:48 +0200

    add typed animation on data right

--- web/js/interactions.js
+++ web/js/interactions.js
@@ -48,12 +48,22 @@ $(document).ready(function() {
     };
 
     var indice,
+        timer1,
+        timer2,
         totalProg = 0,
         totalExplo = 0,
         input = $(".filter__item input"),
         inputChecked = [],
         dataProg = $('.data-prog'),
-        dataExplo = $('.data-explo');
+        dataExplo = $('.data-explo'),
+        newProg_text = "",
+        newExplo_text = "",
+        totalProg_text = "",
+        totalExplo_text = "",
+        totalProg_length = 0,
+        totalExplo_length = 0,
+        i = 0,
+        j = 0;
 
     dataProg.html(nbProg[6]);
     dataExplo.html(nbExplo[6]);
@@ -65,31 +75,50 @@ $(document).ready(function() {
     totalExplo = nbExplo[id];
     //console.log("totalProg : "+totalProg+" // totalExplo : "+totalExplo);
 
+    function typed(text1, text2) {
+        totalProg_text = text1.toString();
+        totalExplo_text = text2.toString();
+
+        totalProg_length = totalProg_text.length;
+        totalExplo_length = totalExplo_text.length;
+
+        dataProg.text('');
+        dataExplo.text('');
+        newProg_text = '';
+        newExplo_text = '';
+        i = 0;
+        j = 0;
+
+        timer1 = setInterval(function(){
+            newProg_text += totalProg_text[i];
+            dataProg.text(newProg_text);
+            if(i < totalProg_length-1) { i++; } else { clearInterval(timer1); }
+        }, 50);
+
+        timer2 = setInterval(function(){
+            newExplo_text += totalExplo_text[j];
+            dataExplo.text(newExplo_text);
+            if(j < totalExplo_length-1) { j++; } else { clearInterval(timer2); }
+        }, 50);
+    }
+
     input.on("change", function(){
 
         if($(this).is(':checked') == true) {
             inputChecked.push($(this).attr('id'));
-            //console.log(inputChecked);
 
             totalProg += nbProg[$(this).attr('id')];
             totalExplo += nbExplo[$(this).attr('id')];
 
-            dataProg.html(totalProg);
-            dataExplo.html(totalExplo);
-
-            //console.log("totalProg : "+totalProg+" // totalExplo : "+totalExplo);
+            typed(totalProg, totalExplo);
         } else {
             indice = inputChecked.indexOf($(this).attr('id'));
             inputChecked.splice(indice, 1);
-            //console.log(inputChecked);
 
             totalProg -= nbProg[$(this).attr('id')];
             totalExplo -= nbExplo[$(this).attr('id')];
 
-            dataProg.html(totalProg);
-            dataExplo.html(totalExplo);
-
-            //console.log("totalProg : "+totalProg+" // totalExplo : "+totalExplo);
+            typed(totalProg, totalExplo);
         }
     });
 
